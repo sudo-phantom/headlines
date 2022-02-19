1269b493cd4451c511417bdc8019000dea3e46ac
sudo-phantom <sudophantom@gmail.com>
2022-02-19 02:48:58 -0700

    fixed error feed

--- headlines.py
+++ headlines.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 	     'cnn': 'http://rss.cnn.com/rss/edition.rss',
 	     'fox': 'http://feeds.foxnews.com/foxnews/latest',
-	     'iol': 'http://www.iol.co.za/cmlink/1.640'
-}
+	     'hackernews': 'http://news.ycombinator.com/rss'}
 #BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 
 @app.route("/")
